79338eccd0b21dc71af9079ed7a60f2f59a93934
Jehiah Czebotar <jehiah@gmail.com>
2010-08-11 23:05:27 -0400

    code to parse daily daily per-station counts

---
+++ scripts/complete_dump_to_daily_files.py
@@ -0,0 +1,100 @@
+"""
+This takes a mta .txt data dump, and normalizes it to a single set of columns so it's a file that can be combined/sorted with other data sets
+"""
+
+import sys
+import os
+import csv
+import logging
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import tornado.options
+
+def load_stations(station_file):
+    input_file = open(station_file, 'rb')
+    reader = csv.reader(input_file)
+    data = {}
+    for row in reader:
+        if not row or row[0] == 'unit':
+            continue
+        control_unit = row[0]
+        station = row[-1]
+        data[control_unit] = station
+    input_file.close()
+    return data
+
+def run(input_filename, output_filename, stations_lookup):
+    logging.info('reading %s' % input_filename)
+    logging.info('outputing to %s' % output_filename)
+    input_file = open(input_filename, 'rb')
+    reader = csv.reader(input_file)
+    
+    # read the data getting only midnight entries
+    data = {}
+    for row in reader:
+        if not row:
+            continue
+        time_str = row[4]
+        if time_str != "00:00:00":
+            continue
+        key = ','.join(row[:3])
+        values = [row[3], int(row[-2]), int(row[-1])]
+        if key not in data:
+            data[key] = []
+        data[key].append(values)
+    
+    # now summarize each key to a daily total (ie: diff the previous day)
+    daily_key_totals = {}
+    for key, data_series in data.items():
+        daily_key_totals[key] = []
+        data_series.sort()
+        for i in range(len(data_series)-1):
+            date, entrances, exits = data_series[i]
+            next_date, next_entrances, next_exits = data_series[i+1]
+            daily_key_totals[key].append((date, next_entrances-entrances, next_exits-exits))
+
+    # finally summarize all stuff to the station level
+    station_totals = {}
+    for key, data_series in daily_key_totals.items():
+        control_area = key.split(',')[1]
+        station = stations_lookup[control_area]
+        if station not in station_totals:
+            station_totals[station] = {}
+            for date, entrances, exits in data_series:
+                if date not in station_totals[station]:
+                    station_totals[station][date] = [entrances, exits]
+                else:
+                    current_entrances, current_exits = station_totals[station][date]
+                    station_totals[station][date] = [current_entrances + entrances, current_exits + exits]
+    
+    # now go through all the dates
+    all_dates = []
+    for station, date_data in station_totals.items():
+        all_dates += date_data.keys()
+    all_dates = list(set(all_dates))
+    all_dates.sort()
+    
+    output_file = open(output_filename, 'ab')
+    writer = csv.writer(output_file)
+    for date in all_dates:
+        for station in station_totals:
+            entrances, exits = station_totals[station].get(date, [0,0])
+            writer.writerow([date, station, str(entrances), str(exits)])
+    
+    output_file.close()
+
+if __name__ == "__main__":
+    tornado.options.define('input_file', type=str, help="input csv file with complete data", default="../data/complete.csv")
+    tornado.options.define('output_file', type=str, help="output file with daily data", default="../data/turnstile_daily.csv")
+    tornado.options.define('stations_file', type=str, help="Remote-Booth-Stations.csv", default="../data/Remote-Booth-Station.csv")
+    tornado.options.parse_command_line()
+
+    input_file = tornado.options.options.input_file
+    stations_file = tornado.options.options.stations_file
+    assert os.path.exists(input_file)
+    assert os.path.exists(stations_file)
+
+    stations = load_stations(stations_file)
+    logging.info('stations lookup %s' % stations)
+    run(input_file, tornado.options.options.output_file, stations)
--- scripts/turnstile_to_csv.py
+++ scripts/turnstile_to_csv.py
@@ -1,3 +1,6 @@
+"""
+This takes a mta .txt data dump, and normalizes it to a single set of columns so it's a file that can be combined/sorted with other data sets
+"""
 
 import sys
 import os
@@ -8,14 +11,24 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 import tornado.options
 
-def run(input_filename, output_file_format):
+def run(input_filename, output_filename):
+    logging.info('reading %s' % input_filename)
+    logging.info('outputing to %s' % output_filename)
     input_file = open(input_filename, 'rb')
+    output_file = open(output_filename, 'ab')
+    writer = csv.writer(output_file)
     reader = csv.reader(input_file)
+    
+    rows = 0
     for row in reader:
         if not row or len(row) < 43:
             continue
         for block in _block(row):
-            logging.info(block)
+            rows += 1
+            writer.writerow(block)
+    logging.info('output %d records' % rows)
+    input_file.close()
+    output_file.close()
     
 def _block(data):
     #C/A,UNIT,SCP,DATE1,TIME1,DESC1,ENTRIES1,EXITS1,DATE2,TIME2,DESC2,ENTRIES2,EXITS2,DATE3,TIME3,DESC3,ENTRIES3,EXITS3,DATE4,TIME4,DESC4,ENTRIES4,EXITS4,DATE5,TIME5,DESC5,ENTRIES5,EXITS5,DATE6,TIME6,DESC6,ENTRIES6,EXITS6,DATE7,TIME7,DESC7,ENTRIES7,EXITS7,DATE8,TIME8,DESC8,ENTRIES8,EXITS8
@@ -31,10 +44,10 @@ def _block(data):
 
 if __name__ == "__main__":
     tornado.options.define('input_file', type=str, help="input turnstile_YYMMDD.txt file")
-    tornado.options.define('output_format', type=str, help="format for output turnstile_YYMMDD.csv file", default="turnstile_%Y%m%d.csv")
+    tornado.options.define('output_file', type=str, help="output to global data file", default="../data/complete.csv")
     tornado.options.parse_command_line()
 
     input_file = tornado.options.options.input_file
     assert os.path.exists(input_file)
 
-    run(input_file, tornado.options.options.output_format)
+    run(input_file, tornado.options.options.output_file)
